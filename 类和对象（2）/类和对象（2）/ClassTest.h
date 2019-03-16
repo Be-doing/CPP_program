be86f4516ca55d7c82f2a288c5df32ca4858a11d
be-doing <1782904648@qq.com>
2019-03-16 19:31:07 +0800

    类和对象

--- 类和对象（2）/类和对象（2）/ClassTest.h
+++ 类和对象（2）/类和对象（2）/ClassTest.h
@@ -17,6 +17,7 @@ public:
 	//{}
 	//拷贝构造
 	Date(const Date& date)
+		//初始化列表
 		//:ra(10)
 	{
 		year_ = date.year_;
@@ -29,7 +30,7 @@ public:
 	{
 		return year_ == d2.year_&&month_ == d2.month_&&day_ == d2.day_;
 	}
-		//赋值运算符
+	//赋值运算符
 	Date& operator = (const Date& d)
 	{
 		if (this != &d)
@@ -44,6 +45,14 @@ public:
 	{
 		cout << year_ << "/" << month_ << "/" << day_ << endl;
 	}
+	//有缘声明
+	friend  ostream& operator << (ostream& _cout, Date& d);
+	friend istream& operator>>(istream& _cin, const Date& d);
+	//重载 << 定义的类是第一个参数，所以调用的时候需要 date << cout;
+	void operator << (ostream& _cout)
+	{
+		_cout << year_ << "/" << month_ << "/" << day_ << endl;
+	}
 	//const 修饰成员函数
 	void display2()	const
 	{
@@ -53,33 +62,66 @@ private:
 	int year_;
 	int month_;
 	int day_;
-
+	//static成员函数
 	static int hour_;
 };
+//为了是数据私有化，保证数据是安全的，声明为友元
+ostream& operator << (ostream& _cout, Date& d)
+{
+	_cout << d.year_ << "/" << d.month_ << "/" << d.day_ << endl;
+	return _cout;
+
+}
+istream& operator>>(istream& _cin, const Date& d)
+{
+	_cin >> d.year_;
+	_cin >> d.month_;
+	_cin >> d.day_;
+
+	return _cin;
+}
 int Date::hour_ = 12;
 
-//class String 
-//{
-//public:
-//	String(const char* str = "moren")
-//	{
-//		str_ = (char*)malloc(strlen(str) + 1);
-//		strcpy(str_, str);
-//	}
-//	//String()
-//	//{
-//	//	
-//	//}
-//	//析构函数
-//	~String()
-//	{
-//		cout << "正在清空资源" << endl;
-//		free(str_);
-//	}
-//	void display()
-//	{
-//		cout << str_ << endl;
-//	}
-//private:
-//	char* str_;
-//};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class String 
+{
+public:
+	String(const char* str = "moren")
+	{
+		str_ = (char*)malloc(strlen(str) + 1);
+		strcpy(str_, str);
+	}
+	//String()
+	//{
+	//	
+	//}
+	//析构函数
+	~String()
+	{
+		cout << "正在清空资源" << endl;
+		free(str_);
+	}
+	void display()
+	{
+		cout << str_ << endl;
+	}
+private:
+	char* str_;
+};
