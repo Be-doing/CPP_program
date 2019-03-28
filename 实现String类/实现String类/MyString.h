8452a7e0685ac0ae6186436cb5537c6b419f57e4
be-doing <1782904648@qq.com>
2019-03-28 23:27:30 +0800

    待续

--- 实现String类/实现String类/MyString.h
+++ 实现String类/实现String类/MyString.h
@@ -1,7 +1,35 @@
 #pragma once
 #include<iostream>
+#include<assert.h>
 using namespace std;
+class String
+{
+public:
+	String(const char* str = "")
+	{
+		// 构造string类对象时，如果传递nullptr指针，认为程序非法，此处断言下
+		if (nullptr == str)
+		{
+			assert(false);
+			return;
+		}
 
+		str_ = new char[strlen(str) + 1];
+		strcpy(str_, str);
+	}
+
+	~String()
+	{
+		if (str_)
+		{
+			delete[] str_;
+			str_ = nullptr;
+		}
+	}
+
+private:
+	char* str_;
+};
 class Solution
 {
 public:
