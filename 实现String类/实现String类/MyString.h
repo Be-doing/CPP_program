306eb2824773f05220ac715c13df1d6c915844f0
be-doing <1782904648@qq.com>
2019-03-30 23:41:29 +0800

    待续

--- 实现String类/实现String类/MyString.h
+++ 实现String类/实现String类/MyString.h
@@ -35,7 +35,50 @@ public:
 			delete[] str_;
 			str_ = pStr;
 		}
+		String(const char* str = "")
+		{
+			if (nullptr == str)
+				str = "";
 
+			_str = new char[strlen(str) + 1];
+			strcpy(_str, str);
+		}
+
+		String(const String& s)
+			: _str(nullptr)
+		{
+			String strTmp(s._str);
+			swap(_str, strTmp);
+		}
+
+		// 对比下和上面的赋值那个实现比较好？
+		String& operator=(String s)
+		{
+			swap(_str, s._str);
+			return *this;
+		}
+
+		/*
+		String& operator=(const String& s)
+		{
+		if(this != &s)
+		{
+		String strTmp(s);
+		swap(_str, strTmp._str);
+		}
+
+		return *this;
+		}
+		*/
+
+		~String()
+		{
+			if (_str)
+			{
+				delete[] _str;
+				_str = nullptr;
+			}
+		}
 		return *this;
 	}
 
