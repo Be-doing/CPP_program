269acc6d258d5ee0618bbebb73523d0a4efb2a73
be-doing <1782904648@qq.com>
2019-04-09 00:00:41 +0800

    待续

---
+++ vector的使用/vector的使用/vectorTest.cpp
@@ -0,0 +1,69 @@
+#include<vector>
+#include<iostream>
+#include<string>
+using namespace std;
+#include"vector的使用.h"
+template <class T1>
+void Print(vector<T1>& v)
+{
+	for (auto& e : v)
+	{
+		cout << e << "\t";
+	}
+	cout << endl;
+}
+
+//构造函数的使用
+void CreateTest()
+{
+	std::vector<int> v1;
+	std::vector<int> v2(10, 10);
+	std::vector<int> v3(v2);
+	std::vector<int> v4(v2.begin(), v2.end() - 5);
+
+	Print(v1);
+	Print(v2);
+	Print(v3);
+	Print(v4);
+}
+string letterMap[10] = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+void combineStr(const string& digits, size_t index, const string& str, vector<string>& strs)
+{
+	
+	if (index == digits.size())//走到最后一层，保存组成的字符串
+	{
+		strs.push_back(str);
+		return;
+	}
+	// 获取数字对应的字符数组
+	string letters = letterMap[digits[index] - '0'];
+	for (size_t i = 0; i < letters.size(); ++i)
+	{
+		combineStr(digits, index + 1, str + letters[i], strs);
+	}
+}
+std::vector<string> letterCombinations(string digits) {
+	std::vector<string> strs;
+
+	if (digits.empty())
+		return strs;
+
+	size_t index = 0;
+	string str;
+	// 递归子问题组合字符串
+	combineStr(digits, index, str, strs);
+
+	return strs;
+}
+
+int main()
+{
+	//CreateTest();
+	std::vector<string> s;
+	s = letterCombinations("234");
+
+	Print(s);
+	
+	system("pause");
+	return 0;
+}
---
+++ vector的使用/vector的使用/vector的使用.h
@@ -0,0 +1,85 @@
+#pragma once
+#include<vector>
+#include<iostream>
+#include<bitset>
+using namespace std;
+class Solution
+{
+public:
+	//只出现一次的数字
+	int SingleNumber(vector<int>& nums)//其他数都是成对存在，有一个数只有一个
+	{
+		//0 ^ x == x
+		//x ^ x == 0
+		//异或与顺序无关
+		int tmp = 0;
+		for (const auto& e : nums)
+		{
+			tmp ^= e;
+		}
+		return tmp;
+	}
+	int SingleNumber2(vector<int>& nums)//其他数都是成三个存在，有一个数只有一个
+	{
+		char  bitarr[32] = { 0 };
+		for (const auto& e : nums)
+		{
+			for (size_t i = 0; i < 32; i++)
+			{
+				if ((e >> i) & 1 == 1)
+				{
+					bitarr[i] += 1;
+				}
+			}
+		}
+		for (size_t i = 0; i < 32; ++i)
+		{
+			if (bitarr[i] >=3)
+			{
+				bitarr[i] %= 3;
+			}
+		}
+		int temp = 0;
+		for (size_t i = 31; i  >= 0; i--)
+		{
+			temp |= ((bitarr[i] - '0') << i);
+		}
+		return temp;
+	}
+	//给定一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。
+	//	找出只出现一次的那两个元素
+	int* SingleNumber3(vector<int>& nums)
+	{
+		int res[2] = { 0 };
+		int ret = 0;
+		int pos = 0;
+		for (auto& e : nums)
+		{
+			ret ^= e;	
+		}
+		for (size_t i = 0; i < 32; ++i)
+		{
+			if (((ret >> i) & 1) == 1)
+			{
+				pos = i;
+				break;
+			}
+		}
+		for (auto& e : nums)
+		{
+			if (((e >> pos) & 1) == 1)
+			{
+				res[0] ^= e;
+			}
+			else
+			{
+				res[1] ^= e;
+			}
+		}
+	}
+	//数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+	//	例如输入一个长度为9的数组{ 1,2,3,2,2,2,5,4,2 }。由于数字2在数组中出现了5次，
+	//	超过数组长度的一半，因此输出2。如果不存在则输出0。
+
+	//连续子数组的最大和 OJ
+};
