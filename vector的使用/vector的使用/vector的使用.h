f85fb67861079c33d9e238de25b37c906375b039
be-doing <1782904648@qq.com>
2019-04-10 23:48:46 +0800

    待续

--- vector的使用/vector的使用/vector的使用.h
+++ vector的使用/vector的使用/vector的使用.h
@@ -80,6 +80,36 @@ public:
 	//数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
 	//	例如输入一个长度为9的数组{ 1,2,3,2,2,2,5,4,2 }。由于数字2在数组中出现了5次，
 	//	超过数组长度的一半，因此输出2。如果不存在则输出0。
+	int FindNum(vector<int> & nums)
+	{
+		int len = nums.size();
+		int res[2] = { 0 };
+		int ret = 0;
+		int pos = 0;
+		for (auto& e : nums)
+		{
+			ret ^= e;
+		}
+		for (size_t i = 0; i < 32; ++i)
+		{
+			if (((ret >> i) & 1) == 1)
+			{
+				pos = i;
+				break;
+			}
+		}
+		for (auto& e : nums)
+		{
+			if (((e >> pos) & 1) == 1)
+			{
+				res[0] ^= e;
+			}
+			else
+			{
+				res[1] ^= e;
+			}
 
+
+	}
 	//连续子数组的最大和 OJ
 };
