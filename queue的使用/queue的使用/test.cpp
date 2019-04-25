baf50ca411bbce7db7a4f24707934ae383fdc80f
be-doing <1782904648@qq.com>
2019-04-25 23:01:46 +0800

    待续

--- queue的使用/queue的使用/test.cpp
+++ queue的使用/queue的使用/test.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<queue>
 #include<list>
 using namespace std;
 
@@ -32,7 +34,41 @@ private:
 };
 
 
+  struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+ 
+class Solution {
+public:
+	int treehigh(TreeNode* root)
+	{
+		if (!root)
+		{
+			return 0;
+		}
+		int left_ = treehigh(root->left);
+		int right_ = treehigh(root->right);
 
+		return left > right ? left + 1 : right_ + 1;
+	}
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>> treeVec;
+		int height = treehigh(root);
+		queue<TreeNode*> qTree;
+		TreeNode* tmp = root;
+		if (!tmp)
+		{
+			return treeVec;
+		}
+		qTree.push(tmp);
+		while (!qTree.empty())
+		{
+		}
+	}
+};
 int main()
 {
 	Queue<int, list<int>> q1;
