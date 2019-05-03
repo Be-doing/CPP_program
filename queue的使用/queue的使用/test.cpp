8fd186962a335769671dc01b89cd76cab0cdaabe
be-doing <1782904648@qq.com>
2019-05-03 23:33:10 +0800

    queue的OJ

--- queue的使用/queue的使用/test.cpp
+++ queue的使用/queue的使用/test.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<stack>
 #include<list>
 using namespace std;
 
@@ -150,6 +151,46 @@ public:
 			--num2;
 		}
 		return res;
+	}
+};
+class Solution3 {
+public:
+	int evalRPN(vector<string>& tokens) {
+		stack<int> s;
+		for (size_t i = 0; i < tokens.size(); ++i)
+		{
+			string& str = tokens[i];
+			// str为数字
+			if (!("+" == str || "-" == str || "*" == str || "/" == str))
+			{
+				s.push(atoi(str.c_str()));
+			}
+			else
+			{
+				// str为操作符
+				int right = s.top();
+				s.pop();
+				int left = s.top();
+				s.pop();
+				switch (str[0])
+				{
+				case '+':
+					s.push(left + right);
+					break;
+				case '-':
+					s.push(left - right);
+					break;
+				case '*':
+					s.push(left * right);
+					break;
+				case '/':
+					// 题目说明了不存在除数为0的情况
+					s.push(left / right);
+					break;
+				}
+			}
+		}
+		return s.top();
 	}
 };
 int main()
