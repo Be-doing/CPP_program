20286af084bbe50025071633dfc5388a2e3e768a
be-doing <1782904648@qq.com>
2019-05-16 23:19:41 +0800

    io操作

--- C++的io操作/C++的io操作/源.cpp
+++ C++的io操作/C++的io操作/源.cpp
@@ -1,38 +1,39 @@
 #include<iostream>
 #include<fstream>
+#include<string>
 using namespace std;
 
 //cin和cout/Cerro/clog本质是一个对象，每一个对象都进行了重载
 //每个类型调用都调用相应的函数
 
-void Practice()
+void Practice1()
 {
 	char str[1024];
 	ofstream fout("BinaryCin.txt");
 	fout.write("abcd", 5);
 	ifstream fin("BinaryCin.txt");
-	fin.read(str, 1024);
+	fin.read(str, 5);
 	cout << str << endl;
 }
 void Practice2()
 {
 	ofstream out("ChCin.txt");
-	out << "字符流的写入";
-	out << "字符流的写入";
+	out << "字符流的写入" << endl;
+	out << "字符流的写入" << endl;
 	ifstream in("ChCin.txt");
-	char str[1024];
+	//char buff[100];
+	//while (!in.eof())
+	//{
+	//	in.getline(buff,100);
+	//	cout << buff << endl;
+	//}
+
+	string str;
 	in >> str;
-	cout << str << endl;
+	cout << str;
 }
 int main()
 {
-	//int tmp = 15;
-	//ofstream fout;
-
-	//fout.open("mainBin.txt");
-	//fout.write((char*)&tmp, sizeof(15));
-	//cout << endl;
-	//fout << &tmp;
 	Practice2();
 	system("pause");
 	return 0;
