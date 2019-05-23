d34e42843a296c451eaae34c8b0cf4d8906b326b
be-doing <1782904648@qq.com>
2019-05-23 19:14:11 +0800

    C++的io操作

--- C++的io操作/C++的io操作/源.cpp
+++ C++的io操作/C++的io操作/源.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<iomanip>
 using namespace std;
 
 //cin和cout/Cerro/clog本质是一个对象，每一个对象都进行了重载
@@ -8,33 +9,34 @@ using namespace std;
 
 void Practice1()
 {
-	char str[1024];
+	const char bw[] = "abcde";
 	ofstream fout("BinaryCin.txt");
-	fout.write("abcd", 5);
+	fout.write(bw,50);
+	fout.close();
+
+	char br[50];
 	ifstream fin("BinaryCin.txt");
-	fin.read(str, 5);
-	cout << str << endl;
+	fin.read(br, sizeof(br));
+	cout << br << endl;
 }
+
 void Practice2()
 {
 	ofstream out("ChCin.txt");
 	out << "字符流的写入" << endl;
 	out << "字符流的写入" << endl;
-	ifstream in("ChCin.txt");
-	//char buff[100];
-	//while (!in.eof())
-	//{
-	//	in.getline(buff,100);
-	//	cout << buff << endl;
-	//}
 
+	ifstream in("ChCin.txt");
 	string str;
 	in >> str;
-	cout << str;
+	cout << str << endl;
 }
+
 int main()
 {
 	Practice2();
+	//double num = 3.1415926;
+	//cout << setprecision(8) << num << endl;
 	system("pause");
 	return 0;
 }
