084420195a51e30898f457dec24ea9015fb4242b
be-doing <1782904648@qq.com>
2019-05-31 23:45:54 +0800

    待续

--- 搜索二叉树/搜索二叉树/BSTree.h
+++ 搜索二叉树/搜索二叉树/BSTree.h
@@ -19,11 +19,24 @@ class BTree
 {
 public:
 	typedef BTNode<T> Node;
+	BTree()
+		:root_(nullptr)
+	{}
+	BTree(const BTree<T>& bst)
+	{
+
+	}
+
+	BTree<T>& operator=(const BTree<T>& bst)
+	{
+		
+	}
 	Node* Find(const T& val)
 	{
 		if (root_ == nullptr)
 		{
-			return nullptr;
+			root_ = new Node(val);
+			return root_;
 		}
 		Node* cur = root_;
 		while (cur)
@@ -52,19 +65,150 @@ public:
 		}
 		Node* parent = nullptr;
 		Node* child = root_;
-
+		while (child)
+		{
+			if (child->val_ == val)
+			{
+				return false;
+			}
+			if (child->val_ > val)
+			{
+				parent = child;
+				child = child->left;
+			}
+			else
+			{
+				parent = child;
+				child = child->right;
+			}
+		}
+		child = new Node(val);
+		if (parent->val_ > val)
+		{
+			parent->left = child;
+		}
+		else
+		{
+			parent->right = child;
+		}
 	}
-	bool Erase()
+	bool Erase(const T& val)
 	{
+		//当是空树的时候,不能删除
+		if (root_ == nullptr)
+		{
+			return false;
+		}
+
+		Node* child = root_;
+		Node* parent = nullptr;
+		
+		//不是空树，则寻找该结点
+		while (child)
+		{
+			if (child->val_ == val)
+			{
+				break;
+			}
+			if (child->val_ > val)
+			{
+				parent = child;
+				child = child->left;
+			}
+			else
+			{
+				parent = child;
+				child = child->right;
+			}
+		}
+		//没有找到
+		if (child == nullptr)
+		{
+			return false;
+		}
 		//1、叶子结点时，直接删除，父结点的相应指针置空
-		//	2、删除结点是根节点，且只有一个根节点，根节点置空，删除
-		//	3、不是叶子结点
-		//	无左边孩子？指向右孩子
-		//	无右边孩子？指向左孩子
+		if (child->left == nullptr && child->right == nullptr)
+		{
+			//当不是只有一个结点的时候
+			if (child != root_)
+			{
+				if (parent->val_ > val)
+				{
+					parent->left = nullptr;
+				}
+				else
+				{
+					parent->right = nullptr;
+				}
+			}
+			else//只有一个根节点时
+			{
+				root_ = nullptr;
+			}
+			delete child;
+			child = nullptr;
+		}
+		//	2、不是叶子结点
+		//	无右边孩子，只有左孩子
+		else if (child->right == nullptr)
+		{
+			if (child != root_)
+			{
+				if (parent->val_ > val)
+				{
+					parent->left = child->left;
+				}
+				else
+				{
+					parent->right = child->left;
+				}
+			}
+			else
+			{
+				root_ = nullptr;
+			}
+			delete child;
+			child = nullptr;
+		}
+		//	无左边孩子，只有右孩子
+		else if (child->left == nullptr)
+		{
+			if (child != root_)
+			{
+				if (parent->val_ > val)
+				{
+					parent->left = child->right;
+				}
+				else
+				{
+					parent->right = child->right;
+				}
+			}
+			else
+			{
+				root_ = nullptr;
+			}
+			delete child;
+			child = nullptr;
+		}
 		//	都有
 		//	在左孩子找最大的，或者在右孩子找最小的。才满足二叉搜索树
+		else
+		{
+			Node* leftMost = child->left;
+			parent = cur;//防止leftMost没有孩子结点
+			while (leftMost->right)
+			{
+				parent = leftMost;
+				leftMost = leftMost->right;//找到最大的一的结点
+			}
+			child->val_ = leftMost->val_;
+			parent->right = leftMost->left;
+			//存在leftMost是左孩子，没有右孩子的情况
+			delete leftMost;
+			leftMost = nullptr;
+		}
 	}
-
 private:
 	Node* root_;
 };
