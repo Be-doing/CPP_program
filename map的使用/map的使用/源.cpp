ac1f2d430ec5b01baf8f56b2528ee0951f77e46f
be-doing <1782904648@qq.com>
2019-07-21 23:56:16 +0800

    map的使用

--- map的使用/map的使用/源.cpp
+++ map的使用/map的使用/源.cpp
@@ -5,32 +5,49 @@ using namespace std;
 void test1()
 {
 	map<int, string> mp1;
-	mp1.insert(make_pair(1, "陕科大"));
-	mp1.insert(make_pair(2, "西安"));
-
+	mp1.insert(make_pair(2, "大学"));
 	mp1.insert(make_pair(4, "中国"));
 	mp1.insert(make_pair(3, "陕西"));
 	for (const auto& e : mp1)
 	{
 		cout << e.first << "--->" << e.second << endl;
 	}
-	/*****************************************************************/
-	//是否存在这个键值
-	cout << mp1.count(2) << endl;
-	/*****************************************************************/
-	//返回第一个大于或等于键值2的迭代器
-	cout << mp1.lower_bound(2)->second << endl;
-	//返回第一个大于键值2的迭代器
-	cout << mp1.upper_bound(2)->second << endl;
-	/*****************************************************************/
-	string str("string");
-	char c = str.at(3);
-	cout << c << endl;
+	mp1[5] = "中学";
+	mp1[4] = "China";
+	cout << "===================" << endl;
+	for (const auto& e : mp1)
+	{
+		cout << e.first << "--->" << e.second << endl;
+	}
+	// C++11 的类表初始化
+	map<string, string> m2{ { "one", "1" },
+	{ "two", "2" }, 
+	{ "three", "3" }, 
+	{ "four", "4" },
+	{ "five", "5" } 
+	};
+	cout << "===================" << endl;
+	for (const auto& e : m2)
+	{
+		cout << e.first << "--->" << e.second << endl;
+	}
+}
+void test2()
+{
+	multimap<int, string> mulp;
+	mulp.insert(make_pair(4, "四"));
+	mulp.insert(make_pair(4, "4"));
+	mulp.insert(make_pair(4, "four"));
+	for (const auto& e : mulp)
+	{
+		cout << e.first << "--->" << e.second << endl;
+	}
+
 }
 int main()
 {
-	test1();
-
+	//test1();
+	test2();
 	system("pause");
 	return 0;
 }
